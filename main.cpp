1c14cd266b54b64a9b4d8dffd03546bfffe17ef1
ToRyPaFyNya <misamiunyanyanya@gmail.com>
2020-05-03 20:52:39 +0400

    commit 6.1

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ using namespace std;
 //test programm--------------------
 int main()
 {
-    cout << "different" << endl;
     cout << "enter x and y" << endl;
     int x, y;
     cin>>x;
     cin>>y;
+
+    cout << "sum x + y = "<< x + y << endl;
     cout << "diff x - y = "<< x - y << endl;
+    cout << "div x / y = "<< x / y << endl;
+
     return 0;
 }
